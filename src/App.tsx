81016c98c6e0202d1fa2032dc65e712939993a5b
namecooo <kazuki.nakamura.develop@gmail.com>
2024-03-25 08:48:57 +0900

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import { useCallback, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { CountButton } from './components/CountButton'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [isChecked, setIsChecked] = useState(false);
-
-  // この関数は再レンダリングの時に再作成されるため、
-  // CountButton の props が変化して、 CountButton が再レンダリングされる
-  // --> useCallback で再レンダリングされても無視することで回避できる
-  const handleSubmit = useCallback(() => {
-    console.log('Submit!!!!!');
-  }, [])
-
   return (
     <>
       <div>
@@ -25,22 +14,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <CountButton handleClick={handleSubmit}/>
-        <p>
-          Status: {isChecked ? 'Checked!!!' : '...'}
-        </p>
-        <div>
-          <label>
-            <input type="checkbox" onChange={() => setIsChecked((prev) => !prev)}/> Dark Mode
-          </label>
-        </div>
-      </div>
+      <div className="card">{/* --- */}</div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
